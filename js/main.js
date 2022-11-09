2bc5b4c6fbe8e85bb1f4ab4879d6e4aaa138753f
Ivan <i.podenko@gmail.com>
2022-11-09 22:09:17 +0300

    my version

--- js/main.js
+++ js/main.js
@@ -18,21 +18,21 @@ closeBurger.addEventListener("click", function(){
 });
 
 
-gsap.from (".hero__title", {y:60, duration:1.5,ease:"circ.out"});
-gsap.from(".hero__descr", {opacity: 0, delay: 0.7, duration: 2.5,});
-gsap.from (".hero__btn", {y:60,  duration:1.5, ease:"circ.out"});
-gsap.from ("#photo-1", {opacity: 0, delay: 0.9, duration:2});
-gsap.from ("#photo-2", {opacity: 0, delay: 1.3, duration:2});
-gsap.from ("#photo-3", {opacity: 0, delay: 1.8, duration:2});
-gsap.from (".photos__author", {opacity: 0, delay: 1.9, duration:2,ease:"circ.out"});
+// gsap.from (".hero__title", {y:60, duration:1.5,ease:"circ.out"});
+// gsap.from(".hero__descr", {opacity: 0, delay: 0.7, duration: 2.5,});
+// gsap.from (".hero__btn", {y:60,  duration:1.5, ease:"circ.out"});
+// gsap.from ("#photo-1", {opacity: 0, delay: 0.9, duration:2});
+// gsap.from ("#photo-2", {opacity: 0, delay: 1.3, duration:2});
+// gsap.from ("#photo-3", {opacity: 0, delay: 1.8, duration:2});
+// gsap.from (".photos__author", {opacity: 0, delay: 1.9, duration:2,ease:"circ.out"});
 
 // мне больше нравиться вот так
-// gsap.from (".hero__title", {y:60, duration:1.5,ease:"circ.out"});
-// gsap.from (".hero__descr", {y:60, opasity:0, scale:0,  duration:1.5,});
-// gsap.from (".hero__btn", {y:60,  duration:1.5, ease:"circ.out"});
-// gsap.from ("#photo-1", {opacity: 0, delay: 0.9, rotation:380, ease: "bounce.out", duration:2});
-// gsap.from ("#photo-2", {opacity: 0, delay: 1.3, rotation:560, duration:2, ease: "bounce.out"});
-// gsap.from ("#photo-3", {opacity: 0, delay: 1.8, rotation:740, duration:2, ease: "bounce.out"});
+gsap.from (".hero__title", {y:60, duration:1.5,ease:"circ.out"});
+gsap.from (".hero__descr", {y:60, opasity:0, scale:0,  duration:1.5,});
+gsap.from (".hero__btn", {y:60,  duration:1.5, ease:"circ.out"});
+gsap.from ("#photo-1", {opacity: 0, delay: 0.9, rotation:380, ease: "bounce.out", duration:2});
+gsap.from ("#photo-2", {opacity: 0, delay: 1.3, rotation:560, duration:2, ease: "bounce.out"});
+gsap.from ("#photo-3", {opacity: 0, delay: 1.8, rotation:740, duration:2, ease: "bounce.out"});
 
 
 // scale: увеличение
